5373d2cc13539fcc633c78fc8053152a5999dfc0
MARIJN\marij <s1190252@student.windesheim.nl>
2022-12-22 08:52:07 +0100

    min max kortingscode invullen

--- Kortingscode.php
+++ Kortingscode.php
@@ -12,7 +12,7 @@ include "database.php";
 
 <form method="get" action="Kortingscode.php">
     Naam: <input type="text" name="Naam" value="<?php if(empty($_GET['Naam'])){print("");}    else{print($_GET['Naam']);} ?>"> <br>
-    Aantal procent korting: <input type="text" name="KortingPercentage" value="<?php if(empty($_GET['KortingPercentage'])){print("");}    else{print($_GET['KortingPercentage']);} ?>"> <br>
+    Aantal procent korting: <input type="number" name="KortingPercentage" min="0" max="100" value="<?php if(empty($_GET['KortingPercentage'])){print("");}    else{print($_GET['KortingPercentage']);} ?>"> <br>
     <input type="submit" name="AanmakenCode" value="Aanmaken Code">
 
 </form>
